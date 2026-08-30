dc569da59faeaa9a2f30da3011b31a352421ed13
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-08-30 16:59:50 +0530

    nothing just fun

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -91,7 +91,7 @@ LANGUAGE_CODE = "en-in"
 TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
-
+#-----------------------------[Static and Media Files]----------------
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
