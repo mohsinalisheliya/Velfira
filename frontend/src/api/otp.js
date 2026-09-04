87fdd1b4928451eabe67a75dfcd30aa81edb3a61
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-04 17:50:13 +0530

    auto-save: progress update (04-Sep-2026 05:50 PM)

--- frontend/src/api/otp.js
+++ frontend/src/api/otp.js
@@ -3,5 +3,6 @@ import axiosClient from "./axiosClient";
 export const sendOtp = (mobile_number) =>
   axiosClient.post("/auth/otp/send/", { mobile_number });
 
-export const verifyOtp = (mobile_number, otp) =>
-  axiosClient.post("/auth/otp/verify/", { mobile_number, otp });
+// Teesra parameter profileData add kiya aur spread (...) kar diya
+export const verifyOtp = (mobile_number, otp, profileData = {}) =>
+  axiosClient.post("/auth/otp/verify/", { mobile_number, otp, ...profileData });
