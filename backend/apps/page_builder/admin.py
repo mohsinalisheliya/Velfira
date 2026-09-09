b3a93f8fa899e9c12cd12b5b214668f3c53b1511
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-09 14:58:39 +0530

    auto-save: progress update (09-Sep-2026 02:58 PM)

--- backend/apps/page_builder/admin.py
+++ backend/apps/page_builder/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
-all_models = [PageLayout, PageBlock]
-for model in all_models:
-    admin.site.register(model)
+class PageBlockInline(admin.TabularInline):
+    model = PageBlock
+    extra = 1
+
+
+@admin.register(PageLayout)
+class PageLayoutAdmin(admin.ModelAdmin):
+    list_display = ("page_key", "status", "updated_at")
+    inlines = [PageBlockInline]
