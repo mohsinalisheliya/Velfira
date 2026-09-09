de70fdd223dc670696bc499d566a12c280633748
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-09 15:00:17 +0530

    auto-save: progress update (09-Sep-2026 03:00 PM)

--- backend/apps/themes/admin.py
+++ backend/apps/themes/admin.py
@@ -4,5 +4,5 @@ from .models import *
 
 @admin.register(Theme)
 class ThemeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_active', 'start_date', 'end_date')
-    list_filter = ('is_active',)
+    list_display = ("name", "primary_color", "accent_color", "is_active", "start_date", "end_date")
+    list_editable = ("is_active",)
