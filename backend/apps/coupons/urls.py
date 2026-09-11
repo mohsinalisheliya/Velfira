6ffab70bb0a93a925bad385821807df4f36e7be9
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-11 17:22:47 +0530

    auto-save: progress update (11-Sep-2026 05:22 PM)

--- backend/apps/coupons/urls.py
+++ backend/apps/coupons/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ApplyCouponView
+from .views import *
 
 app_name = "coupons"
 
 urlpatterns = [
     path("coupons/apply/", ApplyCouponView.as_view(), name="coupon-apply"),
+    path("admin/coupons/", AdminCouponListCreateView.as_view(), name="admin-coupon-list"),
+path("admin/coupons/<int:pk>/", AdminCouponDetailView.as_view(), name="admin-coupon-detail"),
 ]
