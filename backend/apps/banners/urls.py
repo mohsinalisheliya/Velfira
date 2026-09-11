b1412e42ebb7e7aee3ba37cb4d600a5e8dbe4b96
Sheliya Mohsinali <sheliyamohsin342@gmail.com>
2026-09-11 18:14:22 +0530

    auto-save: progress update (11-Sep-2026 06:14 PM)

--- backend/apps/banners/urls.py
+++ backend/apps/banners/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ActiveBannerListView, AdminBannerListCreateView, AdminBannerDetailView
+from .views import *
 
 app_name = "banners"
 urlpatterns = [
